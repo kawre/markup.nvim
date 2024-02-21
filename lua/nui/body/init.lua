412a26892a23b395dd9579f1b36bda1df9a7c730
kawre <kenterq@gmail.com>
2024-02-21 23:15:39 +0100

    feat(body): modifiable portal

--- lua/nui/body/init.lua
+++ lua/nui/body/init.lua
@@ -2,6 +2,8 @@ local Block = require("nui.block")
 
 ---@class NuiBodyData : NuiBlockData
 ---@field line_index integer
+---@field bufnr integer
+---@field winid integer
 
 ---@class NuiBody : NuiBlock
 ---@field _ NuiBodyData
@@ -14,6 +16,24 @@ function Body:get_line_idx(inc)
   return index
 end
 
+---@param fn function
+function Body:modifiable_portal(fn)
+  local bufnr = self.bufnr
+  if not (bufnr and vim.api.nvim_buf_is_valid(bufnr)) then
+    return
+  end
+
+  local opts = { buf = bufnr }
+  local modi = vim.api.nvim_get_option_value("modifiable", opts)
+  if not modi then
+    vim.api.nvim_set_option_value("modifiable", true, opts)
+  end
+  fn()
+  if not modi then
+    vim.api.nvim_set_option_value("modifiable", false, opts)
+  end
+end
+
 function Body:draw(data)
   self.bufnr = data.bufnr
   self.win_id = data.winid
@@ -21,9 +41,11 @@ function Body:draw(data)
 
   self._.line_index = 1
 
-  for _, element in ipairs(content) do
-    element:draw(self)
-  end
+  self:modifiable_portal(function()
+    for _, element in ipairs(content) do
+      element:draw(self)
+    end
+  end)
 end
 
 function Body:init() --
